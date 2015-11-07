adc145127ad058061073d0a287429b932729ffd3
hecomi <info@hecomi.com>
2015-11-08 02:46:20 +0900

    remove unused file.

--- Scripts/Core/Command.cs
+++ Scripts/Core/Command.cs
@@ -1,10 +1,44 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace uREPL
 {
 
-static public class Commands
+[AttributeUsage(
+	AttributeTargets.Method,
+	AllowMultiple = false,
+	Inherited = false
+)]
+public sealed class CommandAttribute : Attribute
+{
+    public string description;
+	public string command;
+
+    public CommandAttribute(string description)
+	{
+		this.description = description;
+	}
+}
+
+public class CommandInfo
+{
+	public string methodName;
+	public string className;
+	public string description;
+	public string command;
+
+	public CommandInfo(string className, string methodName, string description, string command)
+	{
+		this.className   = className;
+		this.methodName  = methodName;
+		this.description = description;
+		this.command     = string.IsNullOrEmpty(command) ? methodName : command;
+	}
+}
+
+public static class Commands
 {
 }
 
