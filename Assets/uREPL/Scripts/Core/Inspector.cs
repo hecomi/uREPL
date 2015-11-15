26888e7d043a875a38bd2c550f0f006ae37bc5eb
hecomi <info@hecomi.com>
2015-11-15 12:09:01 +0900

    add component inspector.

---
+++ Assets/uREPL/Scripts/Core/Inspector.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace uREPL
+{
+
+public class FieldItemInfo
+{
+	public string name;
+	public Type type;
+	public object value;
+}
+
+public class ComponentInfo
+{
+	public Component instance;
+	public System.Type type;
+	public string componentName;
+	public string gameObjectPath;
+	public List<FieldItemInfo> fields = new List<FieldItemInfo>();
+}
+
+public class Inspector : MonoBehaviour
+{
+	static private Inspector instance;
+
+	[HeaderAttribute("Views")]
+	public GameObject componentViewPrefab;
+
+	[HeaderAttribute("Items")]
+	public GameObject intItemPrefab;
+	public GameObject floatItemPrefab;
+	public GameObject stringItemPrefab;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	static private readonly List<Type> supportedTypes =
+		new List<Type>() {
+			typeof(int),
+			typeof(float),
+			typeof(string),
+		};
+
+	static public void Inspect<T>(T component) where T : Component
+	{
+		if (component == null) {
+			Log.Warn("given component is null.");
+			return;
+		}
+
+		var componentType = typeof(T);
+		var componentInfo = new ComponentInfo();
+		componentInfo.instance       = component;
+		componentInfo.type           = componentType;
+		componentInfo.componentName  = componentType.FullName;
+		componentInfo.gameObjectPath = component.transform.GetPath();
+
+		var fields = componentType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+		foreach (var field in fields) {
+			var type = field.FieldType;
+			if (!supportedTypes.Contains(type)) continue;
+			var info = new FieldItemInfo();
+			info.type = type;
+			info.name = field.Name;
+			info.value = field.GetValue(component);
+			componentInfo.fields.Add(info);
+		}
+
+		Gui.selected.RunOnNextFrame(() => {
+			Output(componentInfo);
+		});
+	}
+
+	static public void Output(ComponentInfo component)
+	{
+		var obj = Gui.InstantiateInOutputContent(instance.componentViewPrefab);
+		if (obj == null) return;
+
+		var item = obj.GetComponent<ComponentItem>();
+		item.title = string.Format("<b><i>{0}</i></b> ({1})",
+			component.componentName, component.gameObjectPath);
+		foreach (var field in component.fields) {
+			InstantiateFieldItem(component, item, field);
+		}
+	}
+
+	static private void InstantiateFieldItem(
+			ComponentInfo component, ComponentItem view, FieldItemInfo field)
+	{
+		GameObject obj = null;
+
+		if (field.type == typeof(int)) {
+			obj = Instantiate(instance.intItemPrefab);
+		} else if (field.type == typeof(float)) {
+			obj = Instantiate(instance.floatItemPrefab);
+		} else if (field.type == typeof(string)) {
+			obj = Instantiate(instance.intItemPrefab);
+		}
+
+		var item = obj.GetComponent<FieldItem>();
+		item.label         = field.name;
+		item.component     = component.instance;
+		item.componentType = component.type;
+		item.fieldName     = field.name;
+		item.fieldType     = field.type;
+
+		view.Add(obj);
+	}
+
+	[Command]
+	static public void Test()
+	{
+		uREPL.Inspector.Inspect(GameObject.Find("uREPL").GetComponent<uREPL.Gui>());
+	}
+}
+
+}
---
+++ Assets/uREPL/Scripts/GuiParts/ComponentItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace uREPL
+{
+
+public class ComponentItem : MonoBehaviour
+{
+	public Text nameText;
+	public Transform fieldsView;
+	public GameObject noAvailableFieldText;
+
+	public string title
+	{
+		get { return nameText.text;  }
+		set { nameText.text = value; }
+	}
+
+	public void Add(GameObject field)
+	{
+		noAvailableFieldText.SetActive(false);
+		field.transform.SetParent(fieldsView);
+	}
+}
+
+}
