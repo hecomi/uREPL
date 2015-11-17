1893e14a9e852cce766e12b60814fc865f81b5cc
hecomi <info@hecomi.com>
2015-11-18 00:32:58 +0900

    add readonly item filed.

---
+++ Scripts/GuiParts/ReadonlyFieldItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Reflection;
+
+public class ReadonlyFieldItem : FieldItem
+{
+	public Text valueText;
+
+	public override object value
+	{
+		get { return Convert.ChangeType(valueText.text, fieldType);  }
+		protected set { valueText.text = value.ToString(); }
+	}
+
+	void Update()
+	{
+		value = componentType.GetField(fieldName).GetValue(component);
+	}
+}
