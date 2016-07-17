6e309e8485894ee40736b3dc29cb0fa285dd0673
hecomi <info@hecomi.com>
2016-07-17 18:49:30 +0900

    add comments.

--- Scripts/GuiParts/CommandInputField.cs
+++ Scripts/GuiParts/CommandInputField.cs
@@ -10,9 +10,14 @@ public class CommandInputField : InputField
 {
 	public Gui parentGui { get; set; }
 
+	// To change default behavior and to support multiple gui,
+	// override InputFiled method here.
 	public override void OnUpdateSelected(BaseEventData eventData)
 	{
 		if (!isFocused) return;
+
+		// To support multiple gui, set the window that this input filed is belonging to 
+		// as the current active window.
 		Gui.selected = parentGui;
 
 		bool consumedEvent = false;
@@ -20,14 +25,18 @@ public class CommandInputField : InputField
 		while (Event.PopEvent(e)) {
 			if (e.rawType == EventType.KeyDown) {
 				consumedEvent = true;
-				// Skip
+
+				// Skip because these keys are used for histroy selection.
 				switch (e.keyCode) {
 					case KeyCode.UpArrow:
 					case KeyCode.DownArrow:
 						continue;
 				}
+
 				var shouldContinue = KeyPressed(e);
-				// Prevent finish
+
+				// Prevent finish.
+				// Command submission and cancel are observed in Core.
 				switch (e.keyCode) {
 					case KeyCode.Return:
 					case KeyCode.KeypadEnter:
@@ -35,6 +44,7 @@ public class CommandInputField : InputField
 						shouldContinue = EditState.Continue;
 						break;
 				}
+
 				if (shouldContinue == EditState.Finish) {
 					DeactivateInputField();
 					break;
