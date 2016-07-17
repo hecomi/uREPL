71f49d72efc948b3072dc1d50878e5d6fa8dc97d
hecomi <info@hecomi.com>
2016-07-17 18:26:21 +0900

    move command-related codes from Core to Commands.

--- Scripts/Core/Core.cs
+++ Scripts/Core/Core.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Mono.CSharp;
 
 namespace uREPL
@@ -21,25 +20,6 @@ public class CompileResult
 public static class Core
 {
 	static private bool isInitialized = false;
-
-	internal class ParseBlockResult
-	{
-		public List<string> matches = new List<string>();
-		public string input;
-		public string output;
-		public string pattern;
-		public string placeholder;
-
-		public ParseBlockResult(
-			string input,
-			string pattern,
-			string placeholder)
-		{
-			this.input = this.output = input;
-			this.pattern = pattern;
-			this.placeholder = placeholder;
-		}
-	}
 
 #if UNITY_EDITOR
 	[UnityEditor.MenuItem("Assets/Create/uREPL")]
@@ -87,89 +67,13 @@ public static class Core
 		// #endif
 	}
 
-	static private ParseBlockResult ConvertBlockToPlaceholder(
-		string input,
-		string pattern,
-		string placeholder)
-	{
-		var result = new ParseBlockResult(input, pattern, placeholder);
-
-		var regex = new Regex(pattern);
-		var n = 0;
-		for (var m = regex.Match(input); m.Success; m = m.NextMatch(), ++n) {
-			result.matches.Add(m.Value);
-			result.output = regex.Replace(result.output, string.Format(placeholder, n), 1);
-		}
-
-		return result;
-	}
-
-	static private string ConvertPlaceholderToBlock(
-		string input,
-		ParseBlockResult result)
-	{
-		for (int i = 0; i < result.matches.Count; ++i) {
-			var placeholder = string.Format(result.placeholder, i);
-			input = input.Replace(placeholder, result.matches[i]);
-		}
-
-		return input;
-	}
-
-
-	static private string ConvertIntoCodeIfCommand(string code)
-	{
-		// NOTE: If two or more commands that have a same name are registered,
-		//       the first one will be used here. It is not correct but works well because
-		//       evaluation will be done after expanding the command to the code.
-
-		// To consider commands with spaces, check if the head of the given code is consistent
-		// with any command name. command list is ranked in descending order of the command string length.
-		var commandInfo = Commands.GetAll().FirstOrDefault(
-			x => (code.Length >= x.command.Length) && 
-			     (code.Substring(0, x.command.Length) == x.command));
-		if (commandInfo == null) {
-			return code;
-		}
-
-		// Remove last semicolon.
-		code = code.TrimEnd(';');
-
-		// Remove command and get only arguments.
-		code = code.Substring(commandInfo.command.Length);
-
-		// Store parentheses.
-		var parentheses = ConvertBlockToPlaceholder(code, "\\([^\\)]+\\)", "<%paren{0}%>");
-		code = parentheses.output;
-
-		// Store quatation blocks.
-		var quates = ConvertBlockToPlaceholder(code, "\"[^\"(\\\")]+\"", "<%quate{0}%>");
-		code = quates.output;
-
-		// Split arguments with space.
-		var args = code.Split(new string[] { " " }, System.StringSplitOptions.RemoveEmptyEntries);
-
-		// Convert the command into the code.
-		code  = string.Format("{0}.{1}(", commandInfo.className, commandInfo.methodName);
-		code += string.Join(", ", args);
-		code += ");";
-
-		// Replace temporary quates placeholders to actual expressions.
-		code = ConvertPlaceholderToBlock(code, quates);
-
-		// Replace temporary parentheses placeholders to actual expressions.
-		code = ConvertPlaceholderToBlock(code, parentheses);
-
-		return code;
-	}
-
 	static public CompileResult Evaluate(string code)
 	{
 		var result = new CompileResult();
 		result.code = code;
 
 		// find commands at first and expand it if found.
-		code = ConvertIntoCodeIfCommand(code);
+		code = Commands.ConvertIntoCodeIfCommand(code);
 
 		// if not match, eval the code using Mono.
 		object ret = null;
